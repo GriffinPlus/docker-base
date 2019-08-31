f16feb87dec912056435c956aeea03b3e945d376
Sascha Falk <sascha@falk-online.eu>
2019-08-31 10:14:25 +0200

    Add files for 'base-supervisor' image to 'base-supervisor' folder

---
+++ base-supervisor/run.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+# This script starts a new instance of the griffinplus/base-supervisor container and opens a shell in it.
+# It is useful in cases where some debugging is needed...
+
+docker run -it \
+    --env STARTUP_VERBOSITY=5 \
+    griffinplus/base-supervisor \
+    run-and-enter
